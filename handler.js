51090f882d39e5c08f9dce0b8649271ec0f45e28
Samantha Burke <samanthaburke.uk@gmail.com>
2020-02-03 20:18:08 +0000

    handler.js codealong with James, different get request formats

--- handler.js
+++ handler.js
@@ -1,17 +1,31 @@
 'use strict';
 
-module.exports.tasks = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Hi James, thanks for answering queries on your Sunday!'
-      },
-      null,
-      2
-    ),
-  };
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+app.get('/tasks', function (req, res) {
+  //res.send('Hello World!');
+  res.json({
+    message: 'Is this thing on?',
+  });
+})
+
+module.exports.tasks = serverless(app);
+
+
+// module.exports.tasks = async event => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: 'Hi James, thanks for answering queries on your Sunday!'
+//       },
+//       null,
+//       2
+//     ),
+//   };
+
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+// };
