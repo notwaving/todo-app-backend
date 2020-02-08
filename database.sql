5d1502e173a86c95060ee5b7fe62cc1950348e41
Samantha Burke <samanthaburke.uk@gmail.com>
2020-02-08 15:03:13 +0000

    updates database to accept randomly generated IDs (uuidv4) from app

--- database.sql
+++ database.sql
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `tasks`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tasks` (
-  `taskID` int(11) NOT NULL AUTO_INCREMENT,
+  `taskID` varchar(16) NOT NULL,
   `taskDescription` varchar(255) NOT NULL,
   `taskCategory` varchar(255) DEFAULT NULL,
   `taskCompleted` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`taskID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `tasks` (
 
 LOCK TABLES `tasks` WRITE;
 /*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES (1,'Yoga','Health',0),(2,'Food shop',NULL,0),(3,'Do homework','School',1);
+INSERT INTO `tasks` VALUES ('1','Yoga','Health',0),('2','Food shop',NULL,0),('3','Do homework','School',1);
 /*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-08 12:37:43
+-- Dump completed on 2020-02-08 15:01:35
