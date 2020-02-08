7008be54c1b741dbd8492a2c9f3ad67b718106c5
Samantha Burke <samanthaburke.uk@gmail.com>
2020-02-08 14:26:31 +0000

    attempt to fix 502 Bad Gateway error

--- handler.js
+++ handler.js
@@ -1,10 +1,10 @@
 'use strict';
 
-import uuidv4 from 'uuid/v4';
-
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+
+import uuidv4 from 'uuid/v4';
 
 const tasks = [
   { id: uuidv4(), description: "Do yoga", category: "Health", completed: false },
