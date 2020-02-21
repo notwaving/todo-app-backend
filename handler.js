2336530866be49a9a74ffb0c4a47695e00037079
Samantha Burke <samanthaburke.uk@gmail.com>
2020-02-21 17:43:18 +0000

    sets up external config

--- handler.js
+++ handler.js
@@ -7,10 +7,10 @@ const uuidv4 = require('uuid/v4');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host     : 'XXXX',
-  user     : 'XXXX',
-  password : 'XXXX',
-  database : 'XXXX'
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_SCHEMA
 });
 
 // const tasks = [
